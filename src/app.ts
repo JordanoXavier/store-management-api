25e9d1d6b4f997aaf96281e08d7615acc370b107
JordanoXavier <112017191+JordanoXavier@users.noreply.github.com>
2023-11-27 11:31:18 -0300

    first router

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import express, { Request, Response, NextFunction } from 'express';
+import helloRouter from './routers/helloRouter';
 
 const app = express();
  
 app.use(express.json());
  
-app.use((req: Request, res: Response, next: NextFunction) => {
-    res.send("Hello World");
-})
+app.use('/hello', helloRouter);
  
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
     res.status(500).send(error.message);
